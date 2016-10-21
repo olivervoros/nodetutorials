9fa1bc2763a155af6e6dff4c53d2bed410a45e0f
Oliver Voros <olivervoros@gmail.com>
2016-10-21 14:04:13 +0200

    add and search website functionality

--- searchengine/models/Website.model.js
+++ searchengine/models/Website.model.js
@@ -2,33 +2,33 @@ var mongoose = require('mongoose');
 var searchPlugin = require('mongoose-search-plugin');
 var Schema = mongoose.Schema;
 
-var websiteSchema = new Schema({
-	title: {
-		type: String,
-		required: true
-	}
-	submittedBy: {
-		id: Schema.ObjectId,
-		ref: 'User'
-	},
-	url: {
-		type: String,
-		required: true,
-
-	},
-	description: {
-		type: String,
-		required: true
-	},
-	created_at: {
-		type: Date,
-		default: Date.now
-	}
-
+var WebsiteSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  submittedBy: {
+    id: {
+      type: Schema.ObjectId,
+      ref: 'User'
+    }
+  },
+  url: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  created_at: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-websiteSchema.plugin(searchPlugin, {
-	fields: ['title','url','description'];
-});
+WebsiteSchema.plugin(searchPlugin, {
+  fields: ['title', 'url', 'description']
+})
 
-module.exports = mongoose.model('Website', websiteSchema);
+module.exports = mongoose.model('Website', WebsiteSchema);
--- searchengine/server.js
+++ searchengine/server.js
@@ -11,7 +11,8 @@ var port = 8080;
 var db = 'mongodb://localhost/searchengine';
 mongoose.connect(db);
 
-var users = require('./routes/user')
+var users = require('./routes/user');
+var websites = require('./routes/website');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -31,7 +32,8 @@ require('./config/passport')();
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/users',users);
+app.use('/users', users);
+app.use('/website', websites);
 
 app.listen(port, function() {
 	console.log('App listening on port ' + port);
