c428be72416bb416f59cb644a9df499248521f02
Oliver Voros <olivervoros@gmail.com>
2016-10-22 16:39:16 +0200

    contact form and nodemailer

--- expresssite/app.js
+++ expresssite/app.js
@@ -22,6 +22,40 @@ app.get('/about', function(req, res) {
 	res.render('about');
 });
 
+app.get('/contact', function(req, res) {
+	res.render('contact');
+});
+
+app.post('/contact/send', function(req, res) {
+	var transporter = nodemailer.createTransport({
+		service: 'Gmail',
+		auth: {
+			user: 'email',
+			password: 'password'
+		}
+	});
+
+	var mailOptions = {
+		from: 'TODO',
+		to: 'TODO',
+		subject: "Contact us form submitted",
+		text: "You have a submission with the following details, name: " + req.body.name + " email: " + req.body.email +
+		" message: " + req.body.message,
+		html: "<p>You have a submission with the following details, name: " + req.body.name + " email: " + req.body.email +
+		" message: " + req.body.message + "</p>"
+	};
+
+	transporter.sendMail(mailOptions, function(err, info) {
+		if(err) {
+			console.log('error');
+			res.redirect('/');
+		} else {
+			console.log('Message sent.' + info.response);
+			res.redirect('/');
+		}
+	});
+});
+
 
 app.listen(3000);
 console.log('server is running on port 3000');
