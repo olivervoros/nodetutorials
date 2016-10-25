9f8d0925cff78ec9e6633a85dabbdd7471cecb1d
Oliver Voros <olivervoros@gmail.com>
2016-10-25 13:05:36 +0200

    Form validation

--- nodeauth/routes/users.js
+++ nodeauth/routes/users.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+var upload = multer({ destination: './uploads'});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -8,6 +10,40 @@ router.get('/', function(req, res, next) {
 
 router.get('/register', function(req, res, next) {
   res.render('register', { title: 'Register'} );
+});
+
+router.post('/register', upload.single('profileimage'), function(req, res, next) {
+  var name = req.body.name;
+  var email = req.body.email;
+  var username = req.body.username;
+  var password = req.body.password;
+  var password2 = req.body.password2;
+
+  if(req.file) {
+  	console.log('Uploading file...');
+  	var profileimage = req.file.filename;
+  } else {
+  	console.log('No file uploaded...');
+  	var profileimage = 'noimage.jpg';
+  }
+
+  // Form validation
+  req.checkBody('name' , 'Name field is required').notEmpty();
+  req.checkBody('email' , 'Email field is required').notEmpty();
+  req.checkBody('email' , 'Email is not a valid email address').isEmail();
+  req.checkBody('username' , 'Username field is required').notEmpty();
+  req.checkBody('password' , 'Password field is required').notEmpty();
+  req.checkBody('password2' , 'Passwords do not match').equals(req.body.password);
+
+  // check errors
+  var errors = req.validationErrors();
+
+  if(errors) {
+  	res.render('register', { errors: errors})
+  } else {
+  	console.log('No errors');
+  }
+
 });
 
 router.get('/login', function(req, res, next) {
