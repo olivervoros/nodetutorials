34cb4fbdbe389aa08a9e8c5f561c4f92ecde177c
Oliver Voros <olivervoros@gmail.com>
2016-10-29 20:20:08 +0200

    controllers and models

--- bookstore/controllers/books.js
+++ bookstore/controllers/books.js
@@ -1,5 +1,8 @@
 "use strict";
 
+var Book = require('../models/bookModel');
+var Category = require('../models/categoryModel');
+
 module.exports = function(router) {
 	router.get('/', function(req, res) {
 		res.render('index');
@@ -8,6 +11,17 @@ module.exports = function(router) {
 
 module.exports = function(router) {
 	router.get('/details/:id', function(req, res) {
-		res.render('books/details');
+
+		Book.findOne({_id: req.params.id}, function(err, book) {
+			if(err) {
+				console.log(err);
+			}
+
+			var model = {
+				book: book
+			}
+
+			res.render('books/details', model);
+		})
 	})
 }
