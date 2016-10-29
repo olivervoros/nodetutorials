da0bdfbdb9654f68c35e065ba5015a34a572d9e2
Oliver Voros <olivervoros@gmail.com>
2016-10-29 22:21:03 +0200

    Admin CRUD 1.

--- bookstore/controllers/manage.js
+++ bookstore/controllers/manage.js
@@ -1,12 +1,85 @@
 "use strict";
 
+var Book = require('../models/bookModel');
+var Category = require('../models/categoryModel');
+
 module.exports = function(router) {
 	router.get('/', function(req, res) {
 		res.render('manage/index');
 	});
 
 	router.get('/books', function(req, res) {
-		res.render('manage/books/index');
+		Book.find({}, function(err, books) {
+			if(err) {
+				console.log(err);
+			}
+
+			var model = {
+				books: books
+			};
+
+
+
+			res.render('manage/books/index', model);
+
+		}) ;
+	});
+
+	router.get('/books/add', function(req, res) {
+		Category.find({}, function(err, categories) {
+			if(err) {
+				console.log(err);
+			}
+
+			var model = {
+				categories: categories
+			}
+
+			res.render('manage/books/add', model);
+		})
+	});
+
+	router.post('/books', function(req, res) {
+		var title = req.body.title && req.body.title.trim();
+		var category = req.body.category && req.body.category.trim();
+		var author = req.body.author && req.body.author.trim();
+		var publisher = req.body.publisher && req.body.publisher.trim();
+		var price = req.body.price && req.body.price.trim();
+		var description = req.body.description && req.body.description.trim();
+		var cover = req.body.cover && req.body.cover.trim();
+
+		if(title == '' || price == '') {
+			req.flash('Please fill out the requried fields');
+			res.location('/manage/books/add');
+			res.redirect('/manage/books/add');
+		}
+
+		if(isNaN(price)) {
+			req.flash('Price must be a number!');
+			res.location('/manage/books/add');
+			res.redirect('/manage/books/add');
+		}
+
+		var newBook = new Book({
+			title: title,
+			category: category,
+			author: author,
+			publisher: publisher,
+			price: price,
+			description: description,
+			cover: cover
+		});
+
+		newBook.save(function(err) {
+			if(err) {
+				console.log(err);
+			}
+
+			req.flash('Book has been added.');
+			res.location('/manage/books');
+			res.redirect('/manage/books');
+		});
+
 	});
 
 	router.get('/categories', function(req, res) {
