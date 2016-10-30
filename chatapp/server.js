2073fdef586c79db7b56211e8ed960e4dfb39da3
Oliver Voros <olivervoros@gmail.com>
2016-10-30 18:05:16 +0100

    socket.io chat further development

--- chatapp/server.js
+++ chatapp/server.js
@@ -4,6 +4,9 @@ server = require('http').createServer(app),
 io = require('socket.io').listen(server);
 
 
+var usernames = [];
+
+
 server.listen(process.env.PORT || 3000);
 console.log('Server is running.');
 
@@ -14,7 +17,31 @@ app.get('/', function(req, res) {
 io.sockets.on('connection', function(socket) {
 	console.log('Socket is connected.');
 
+	socket.on('new user', function(data, callback) {
+		if(usernames.indexOf(data) != -1) {
+			callback(false);
+		} else {
+			callback(true);
+			socket.username = data;
+			usernames.push(socket.username);
+			updateUsernames();
+		}
+	});
+
+	function updateUsernames() {
+		io.sockets.emit('usernames', usernames);
+	}
+
 	socket.on('send message', function(data) {
-		io.sockets.emit('new message', {msg: data});
-	})
+		io.sockets.emit('new message', {msg: data, user: socket.username});
+	});
+
+	socket.on('disconnect', function(data) {
+		if(! socket.username) {
+			return;
+		}
+
+		usernames.splice(usernames.indexOf(socket.username), 1);
+		updateUsernames();
+	});
 })
